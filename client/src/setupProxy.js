29ffd5b1b338820608140dca87fd327ec1f7a220
snymanje <jean.snyman@gmail.com>
2021-03-14 16:40:49 +0200

    removed unneeded files

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    ["/api", "/auth/google"],
+    ["/api", "/auth/google", "/profile", "/logout"],
     createProxyMiddleware({
       target: "http://localhost:5000",
     })
